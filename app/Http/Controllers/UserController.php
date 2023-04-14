31633a4d7f945d15604426534c792f7691d44d37
timothyoesch <54284649+timothyoesch@users.noreply.github.com>
2023-04-14 17:25:52 +0200

    Add User Destroy Method

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -82,7 +82,8 @@ class UserController extends Controller
      */
     public function destroy(User $user)
     {
-        //
+        $user->delete();
+        return redirect()->route('users.index');
     }
 
     /**
