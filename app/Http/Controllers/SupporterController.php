2e85211b8d3c29eb7564ed19c589ccbcfd147423
timothyoesch <54284649+timothyoesch@users.noreply.github.com>
2023-04-15 11:43:26 +0200

    Debug Supporter Controller

--- app/Http/Controllers/SupporterController.php
+++ app/Http/Controllers/SupporterController.php
@@ -64,6 +64,7 @@ class SupporterController extends Controller
      */
     public function edit(Site $site, Supporter $supporter)
     {
+        dd($site, $supporter);
         return view("sites.supporters.edit", [
             "site" => $site,
             "supporter" => $supporter
